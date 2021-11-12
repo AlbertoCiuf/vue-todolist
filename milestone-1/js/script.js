0b42bcad35a8bd2ad251536a550b4aa17074bb15
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-12 13:11:05 +0100

    milestone 1: done

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,7 +1,21 @@
 const app = new Vue({
   el: '#app',
   data: {
+    tasks: [
+      {
+        text: 'Studiare Vue.js',
+        done: false
+      },
+      {
+        text: 'Ripassare CSS',
+        done: true
+      },
+      {
+        text: 'Passare un buon weekend',
+        done: false
+      },
 
+    ]
   },
   methods: {
     
