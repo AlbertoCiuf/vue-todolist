8a335e5072a8336d55fe67549f00449aba637533
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-12 16:11:12 +0100

    milestone3: done e bonus fatti

---
+++ milestone-3/js/script.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    tasks: [
+      {
+        text: 'Studiare Vue.js',
+        done: false
+      },
+      {
+        text: 'Ripassare CSS',
+        done: true
+      },
+      {
+        text: 'Passare un buon weekend',
+        done: false
+      },
+    ],
+    newTask:{
+      text:'',
+      done: false
+    },
+    error: false
+  },
+  methods: {
+    removeTask(index){
+      this.tasks.splice(index,1);
+    },
+    addTask(){
+      if (this.newTask.length < 3) {
+        this.error = true;
+        setTimeout(() => {
+          this.error = false;
+        }, 3000);
+      } else {
+        this.tasks.push(this.newTask)
+        this.newTask= {
+          text:'',
+          done: false
+        };
+      } 
+    }
+  }
+});
